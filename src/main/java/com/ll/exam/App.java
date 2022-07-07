8fd1c734f7ba2a64732fd5e1d3bf2756ad7118a3
jinsu291 <wlstn291@naver.com>
2022-07-07 14:47:54 +0900

    명령 프롬프트 구현

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -3,10 +3,22 @@ package com.ll.exam;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
-        new App().run() {
-            System.out.println("== 명언 SSG ==");
+    public void run() {
+        System.out.println("== 명언 SSG ==");
+
+        Scanner sc = new Scanner(System.in);
+
+        outer:
+        while (true) {
+            System.out.printf("명령) ");
+            String cmd = sc.nextLine().trim();
+
+            switch (cmd) {
+                case "종료":
+                    break outer;
+            }
         }
-        Scanner sc =
+
+        sc.close();
     }
 }
