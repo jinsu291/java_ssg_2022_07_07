1e37955347d35c3faa1a3087a176338766ea226e
jinsu291 <wlstn291@naver.com>
2022-07-07 14:38:00 +0900

    세팅

---
+++ src/main/java/com/ll/exam/App.java
@@ -0,0 +1,12 @@
+package com.ll.exam;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        new App().run() {
+            System.out.println("== 명언 SSG ==");
+        }
+        Scanner sc =
+    }
+}
