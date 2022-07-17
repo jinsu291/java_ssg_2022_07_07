2c63e2761d7e6312f4a6ea1366f1e17a8f192c4e
jinsu291 <wlstn291@naver.com>
2022-07-17 22:16:19 +0900

    FileDBTest에 beforeEach 도입

--- src/test/java/com/ll/exam/FileDBTest.java
+++ src/test/java/com/ll/exam/FileDBTest.java
@@ -1,5 +1,6 @@
 package com.ll.exam;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -7,17 +8,20 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FileDBTest {
-    @Test
-    void 파일에_있는_JSON을_객체로_변환(){
+
+    @BeforeEach
+    void beforeEach(){
         Util.deleteDir("test_data");
         Util.mkdir("test_data");
+    }
+
+    @Test
+    void 파일에_있는_JSON을_객체로_변환(){
         WiseSaying wiseSaying = new WiseSaying(1, "내 사전에 불가능은 없다.", "나폴레옹");
         Util.saveToFile("test_data/1.json", wiseSaying.toJson());
-
         String rs = Util.readFromFile("test_data/1.json");
         Map<String, Object> map = Util.jsonToMap(rs);
         WiseSaying loadedWiseSaying = new WiseSaying(map);
-
         assertEquals(1, map.get("id"));
         assertEquals("내 사전에 불가능은 없다.", map.get("content"));
         assertEquals(wiseSaying, loadedWiseSaying);
@@ -25,15 +29,10 @@ public class FileDBTest {
 
     @Test
     void 파일에_있는_JSON을_객체화_맵으로_변환(){
-        Util.deleteDir("test_data");
-        Util.deleteDir("test_data");
-        Util.mkdir("test_data");
         WiseSaying wiseSaying = new WiseSaying(1, "내 사전에 불가능은 없다.", "나폴레옹");
         Util.saveToFile("test_data/1.json", wiseSaying.toJson());
-
         String rs = Util.readFromFile("test_data/1.json");
         Map<String, Object> map = Util.jsonToMap(rs);
-
         assertEquals(1, map.get("id"));
         assertEquals("내 사전에 불가능은 없다.", map.get("content"));
         assertEquals("나폴레옹", map.get("author"));
@@ -41,33 +40,22 @@ public class FileDBTest {
 
     @Test
     void 파일에_객체를_저장(){
-        Util.deleteDir("test_data");
-        Util.mkdir("test_data");
         WiseSaying wiseSaying = new WiseSaying(1, "내 사전에 불가능은 없다.", "나폴레옹");
         Util.saveToFile("test_data/1.json", wiseSaying.toJson());
-
         String rs = Util.readFromFile("test_data/1.json");
-
         assertEquals(wiseSaying.toJson(), rs);
     }
 
     @Test
     void 파일에_내용쓰기(){
-        Util.deleteDir("test_data");
-        Util.mkdir("test_data");
         Util.saveToFile("test_data/1.json", "내용\n내용");
-
         String rs = Util.readFromFile("test_data/1.json");
-
         assertEquals("내용\n내용", rs);
     }
     @Test
     void 파일에_저장된_내용_가져오기(){
-        Util.mkdir("test_data");
         Util.saveToFile("test_data/1.json", "내용");
-
         String body = Util.readFromFile("test_data/1.json");
-
         System.out.println(body);
     }
 }
